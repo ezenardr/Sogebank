6e77ce6364d4128cecf550d6c317aed1f02e0cd9
ezenardr <ezenardrodolphe@gmail.com>
2023-01-13 17:20:53 -0500

    add main.js / configure tabbed component

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const tabContainer = document.querySelector(".operations__tab-container");
+const tabs = document.querySelectorAll(".operations__tab");
+const operationContent = document.querySelectorAll(".operation__content");
+
+tabContainer.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".operations__tab");
+
+    // Guard close - prevent clicking in empty space
+    if (!clicked) return;
+
+    tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+    clicked.classList.add("operations__tab--active");
+
+    operationContent.forEach((c) =>
+        c.classList.remove("operation__content--active")
+    );
+
+    document
+        .querySelector(`.operation__content--${clicked.dataset.tab}`)
+        .classList.add("operation__content--active");
+});
